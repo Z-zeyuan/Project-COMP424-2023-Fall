2632d192f2b6db63d1054a1b15241e63ac35fc1c
edgardesnos <edgar.desnos@mail.mcgill.ca>
2023-12-05 03:52:17 -0500

    Basic working implementation of MCTS

--- agents/student_agent.py
+++ agents/student_agent.py
@@ -1,285 +1,224 @@
-# Student agent: Add your own agent here
 from agents.agent import Agent
 from store import register_agent
 import sys
 import numpy as np
 from copy import deepcopy
 import time
-
-
-def check_valid_step(self, start_pos, end_pos, barrier_dir):
-    """
-    Check if the step the agent takes is valid (reachable and within max steps).
-
-    Parameters
-    ----------
-    start_pos : tuple
-        The start position of the agent.
-    end_pos : np.ndarray
-        The end position of the agent.
-    barrier_dir : int
-        The direction of the barrier.
-    """
-    # Endpoint already has barrier or is border
-    r, c = end_pos
-    if self.chess_board[r, c, barrier_dir]:
-        return False
-    if np.array_equal(start_pos, end_pos):
-        return True
-
-    # Get position of the adversary
-    adv_pos = self.p0_pos if self.turn else self.p1_pos
-
-    # BFS
-    state_queue = [(start_pos, 0)]
-    visited = {tuple(start_pos)}
-    is_reached = False
-    while state_queue and not is_reached:
-        cur_pos, cur_step = state_queue.pop(0)
-        r, c = cur_pos
-        if cur_step == self.max_step:
-            break
-        for dir, move in enumerate(self.moves):
-            if self.chess_board[r, c, dir]:
-                continue
-
-            next_pos = cur_pos + move
-            if np.array_equal(next_pos, adv_pos) or tuple(next_pos) in visited:
-                continue
-            if np.array_equal(next_pos, end_pos):
-                is_reached = True
-                break
-
-            visited.add(tuple(next_pos))
-            state_queue.append((next_pos, cur_step + 1))
-
-    return is_reached
-
-
-def check_endgame(self):
-    """
-    Check if the game ends and compute the current score of the agents.
-
-    Returns
-    -------
-    is_endgame : bool
-        Whether the game ends.
-    player_1_score : int
-        The score of player 1.
-    player_2_score : int
-        The score of player 2.
-    """
-    # Union-Find
-    father = dict()
-    for r in range(self.board_size):
-        for c in range(self.board_size):
-            father[(r, c)] = (r, c)
-
-    def find(pos):
-        if father[pos] != pos:
-            father[pos] = find(father[pos])
-        return father[pos]
-
-    def union(pos1, pos2):
-        father[pos1] = pos2
-
-    for r in range(self.board_size):
-        for c in range(self.board_size):
-            for dir, move in enumerate(
-                    self.moves[1:3]
-            ):  # Only check down and right
-                if self.chess_board[r, c, dir + 1]:
-                    continue
-                pos_a = find((r, c))
-                pos_b = find((r + move[0], c + move[1]))
-                if pos_a != pos_b:
-                    union(pos_a, pos_b)
-
-    for r in range(self.board_size):
-        for c in range(self.board_size):
-            find((r, c))
-    p0_r = find(tuple(self.p0_pos))
-    p1_r = find(tuple(self.p1_pos))
-    p0_score = list(father.values()).count(p0_r)
-    p1_score = list(father.values()).count(p1_r)
-    if p0_r == p1_r:
-        return False, p0_score, p1_score
-    player_win = None
-    win_blocks = -1
-    if p0_score > p1_score:
-        player_win = 0
-        win_blocks = p0_score
-    elif p0_score < p1_score:
-        player_win = 1
-        win_blocks = p1_score
-    else:
-        player_win = -1  # Tie
-    if player_win >= 0:
-        logging.info(
-            f"Game ends! Player {self.player_names[player_win]} wins having control over {win_blocks} blocks!"
-        )
-    else:
-        logging.info("Game ends! It is a Tie!")
-    return True, p0_score, p1_score
-
-
-def check_boundary(self, pos):
-    r, c = pos
-    return 0 <= r < self.board_size and 0 <= c < self.board_size
-
-
-def set_barrier(self, r, c, dir):
-    # Set the barrier to True
-    self.chess_board[r, c, dir] = True
-    # Set the opposite barrier to True
-    move = self.moves[dir]
-    self.chess_board[r + move[0], c + move[1], self.opposites[dir]] = True
-
-def remove_barrier(chess_board, x, y, direction):
-    return
-
-def random_walk(self, my_pos, adv_pos):
-    """
-    Randomly walk to the next position in the board.
-
-    Parameters
-    ----------
-    my_pos : tuple
-        The position of the agent.
-    adv_pos : tuple
-        The position of the adversary.
-    """
-    steps = np.random.randint(0, self.max_step + 1)
-
-    # Pick steps random but allowable moves
-    for _ in range(steps):
-        r, c = my_pos
-
-        # Build a list of the moves we can make
-        allowed_dirs = [d
-                        for d in range(0, 4)  # 4 moves possible
-                        if not self.chess_board[r, c, d] and  # chess_board True means wall
-                        not adv_pos == (r + self.moves[d][0], c + self.moves[d][1])]  # cannot move through Adversary
-
-        if len(allowed_dirs) == 0:
-            # If no possible move, we must be enclosed by our Adversary
-            break
-
-        random_dir = allowed_dirs[np.random.randint(0, len(allowed_dirs))]
-
-        # This is how to update a row,col by the entries in moves
-        # to be consistent with game logic
-        m_r, m_c = self.moves[random_dir]
-        my_pos = (r + m_r, c + m_c)
-
-    # Final portion, pick where to put our new barrier, at random
-    r, c = my_pos
-    # Possibilities, any direction such that chess_board is False
-    allowed_barriers = [i for i in range(0, 4) if not self.chess_board[r, c, i]]
-    # Sanity check, no way to be fully enclosed in a square, else game already ended
-    assert len(allowed_barriers) >= 1
-    dir = allowed_barriers[np.random.randint(0, len(allowed_barriers))]
-
-    return my_pos, dir
-
-
-class MCTS():
-    def __init__(self, chess_board, my_pos, adv_pos, max_step):
-        self.chess_board = chess_board
-        self.board_size = len(self.chess_board)
-        self.my_pos = my_pos
-        self.adv_pos = adv_pos
-        self.max_step = max_step
-
-        self.children = []
-
-    def get_move(self):
-
-        moves = []
-        (r, c), dir = random_walk(self.chess_board, self.my_pos, self.adv_pos)
-        moves.append(((r, c), dir))
-        for i in range(depth):#depth to be tuned
-            (r, c), dir = random_walk(self.chess_board, self.my_pos, self.adv_pos, self.max_step)
-            #set_barrier(self.chess_board, r, c, dir)
-            #check small moves
-
-            if self.chess_board[self.adv_pos[0], self.adv_pos[1]].sum() >= 2: return ((r, c), dir)
-            moves.append(((r, c), dir))
-            #remove_barrier(self.chess_board, r, c, dir)
-
-        #find best move
-        l = []
-        for move in moves:
-            (r, c), dir = move
-            set_barrier(self.chess_board, r, c, dir)
-            #find huristic
-            remove_barrier(self.chess_board, r, c, dir)
-
-        #sort l
-        return l[0][0]
-
-    def expand_children(self):
-        moves = []
-        i = 0
-        #num_child = self.board_size
-        #num_simul = num_child
-        while i < num_child:
-            move = self.get_move()
-            (x, y), direction = move
-            if move in moves: continue
-            set_barrier(self.chess_board, x, y, direction)
-            #get huristic of move
-            remove_barrier(self.chess_board, x, y, direction)
-            self.children.append((huristic, move))
-            i += 1
-
-    def find_move(self):
-        # choose the move with the highest utility
-        self.expand_children()
-        #sort children
-        return self.children[0][1]
-
+import math
+import random
 
 @register_agent("student_agent")
 class StudentAgent(Agent):
     """
-    A dummy class for your implementation. Feel free to use this class to
-    add any helper 0functionalities needed for your agent.
+    Our agent which uses MCTS coupled with a simple heuristic function
     """
 
     def __init__(self):
         super(StudentAgent, self).__init__()
         self.name = "StudentAgent"
-        self.dir_map = {
-            "u": 0,
-            "r": 1,
-            "d": 2,
-            "l": 3,
-        }
+        self.autoplay = True
+
+    def __str__(self) -> str:
+        return self.name
 
     def step(self, chess_board, my_pos, adv_pos, max_step):
-        """
-        Implement the step function of your agent here.
-        You can use the following variables to access the chess board:
-        - chess_board: a numpy array of shape (x_max, y_max, 4)
-        - my_pos: a tuple of (x, y)
-        - adv_pos: a tuple of (x, y)
-        - max_step: an integer
-
-        You should return a tuple of ((x, y), dir),
-        where (x, y) is the next position of your agent and dir is the direction of the wall
-        you want to put on.
-
-        Please check the sample implementation in agents/random_agent.py or agents/human_agent.py for more details.
-        """
-
-        # Some simple code to help you with timing. Consider checking 
-        # time_taken during your search and breaking with the best answer
-        # so far when it nears 2 seconds.
-        start_time = time.time()
-        time_taken = time.time() - start_time
         
-        print("My AI's turn took ", time_taken, "seconds.")
+        root_state = WorldState(chess_board, my_pos, adv_pos, max_step)
+        mcts = MCTS(root_state)
 
-        # dummy return
-        return my_pos, self.dir_map["u"]
+        move = mcts.run(1000)
+
+        return move
+
+
+class WorldState:
+
+    def __init__(self, chess_board, my_pos, adv_pos, max_step):
+        self.chess_board = chess_board
+        self.my_pos = my_pos
+        self.adv_pos = adv_pos
+        self.max_step = max_step
+
+        self.main_agent_turn = True # Means my_pos corresponds to our agent not the adversary
+
+        self.moves = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+        self.opposites = {0: 2, 1: 3, 2: 0, 3: 1}
+        self.board_size = self.chess_board.shape[0]
+
+    def get_possible_moves(self):
+        """Get a list of all possible moves in the given state"""
+        possible_moves = []
+
+        visited = set()
+        queue = [(self.my_pos, 0)]
+
+        while queue:
+            current, steps = queue.pop(0)
+            if current in visited or steps > self.max_step:
+                continue
+
+            r, c = current
+
+            visited.add(current)
+            possible_moves.extend([(current, d) for d in range(0,4) if not self.chess_board[r,c,d]])
+
+            for d in range(0,4):
+                new_position = (r + self.moves[d][0], c + self.moves[d][1])
+                if not self.chess_board[r,c,d] and not self.adv_pos == new_position and new_position not in visited:
+                    queue.append((new_position, steps + 1))
+
+        return possible_moves
+    
+
+    def check_endgame(self):
+        # Union-Find
+        father = dict()
+        for r in range(self.board_size):
+            for c in range(self.board_size):
+                father[(r, c)] = (r, c)
+
+        def find(pos):
+            if father[pos] != pos:
+                father[pos] = find(father[pos])
+            return father[pos]
+
+        def union(pos1, pos2):
+            father[pos1] = pos2
+
+        for r in range(self.board_size):
+            for c in range(self.board_size):
+                for dir, move in enumerate(
+                    self.moves[1:3]
+                ):  # Only check down and right
+                    if self.chess_board[r, c, dir + 1]:
+                        continue
+                    pos_a = find((r, c))
+                    pos_b = find((r + move[0], c + move[1]))
+                    if pos_a != pos_b:
+                        union(pos_a, pos_b)
+
+        for r in range(self.board_size):
+            for c in range(self.board_size):
+                find((r, c))
+        p0_r = find(tuple(self.my_pos))
+        p1_r = find(tuple(self.adv_pos))
+        p0_score = list(father.values()).count(p0_r)
+        p1_score = list(father.values()).count(p1_r)
+        final_score = p0_score - p1_score if self.main_agent_turn else p1_score - p0_score
+        if p0_r == p1_r:
+            return False, final_score
+        return True, final_score
+    
+
+    def get_next_state(self, move):
+        # Assuming the move is legal (should always be the case for our purposes)
+        next_state = deepcopy(self)
+
+        new_pos, new_wall = move
+
+        # Move player to position and place wall
+        next_state.my_pos = new_pos
+        next_state.set_barrier(new_pos[0], new_pos[1], new_wall)
+
+        # Switch whose turn to play it is
+        next_state.my_pos, next_state.adv_pos = next_state.adv_pos, next_state.my_pos
+        next_state.main_agent_turn = not next_state.main_agent_turn
+
+        return next_state
+    
+    def set_barrier(self, r, c, dir):
+        # Set the barrier to True
+        self.chess_board[r, c, dir] = True
+        # Set the opposite barrier to True
+        move = self.moves[dir]
+        self.chess_board[r + move[0], c + move[1], self.opposites[dir]] = True
+
+
+    def get_random_move(self):
+        return random.choice(self.get_possible_moves())
+
+
+class Node:
+
+    def __init__(self, state, parent=None, played_move=None):
+        self.state = state
+        self.parent = parent
+        self.children = []
+        self.visits = 0
+        self.wins = 0
+        self.played_move = played_move
+
+        self.untried_moves = self.state.get_possible_moves()
+
+    def __str__(self):
+        return f"Node: State: my_pos={self.state.my_pos}, adv_pos={self.state.adv_pos}"
+
+    def is_leaf_node(self):
+        return bool(self.children)
+    
+    def is_terminal_node(self):
+        return self.state.check_endgame()[0]
+    
+    def select_child(self, c=1.3):
+        children_ucts = [
+            (child.wins / child.visits) + c * np.sqrt((2 * np.log(self.visits))/child.visits)
+            for child in self.children
+        ]
+        return self.children[np.argmax(children_ucts)]
+
+    def fully_expanded(self):
+        return len(self.untried_moves) == 0
+    
+    def get_untried_move(self):
+        return self.untried_moves.pop(random.randint(0,len(self.untried_moves)-1))
+
+
+
+
+class MCTS:
+
+    def __init__(self, root_state):
+        self.root = Node(root_state)
+
+        self.counter = 0
+
+    def run(self, num_simulations):
+        for _ in range(num_simulations):
+            leaf = self.select(self.root)
+            result = self.simulate(leaf)
+            self.backpropagate(leaf, result)
+
+        return self.root.select_child().played_move
+
+    def select(self, node):
+        curr_node = node
+        while not curr_node.is_terminal_node():
+            if not curr_node.fully_expanded():
+                self.counter += 1
+                return self.expand(curr_node)
+            else:
+                curr_node = curr_node.select_child()
+        return curr_node
+
+    def expand(self, node):
+        new_move = node.get_untried_move()
+        new_state = node.state.get_next_state(new_move)
+        new_node = Node(new_state, parent=node, played_move=new_move)
+        node.children.append(new_node)
+        return new_node
+
+    def simulate(self, node, rollout_depth=10):
+        rollout_state = node.state
+        ended, score = rollout_state.check_endgame()
+        while not ended:
+            move = rollout_state.get_random_move()
+            rollout_state = rollout_state.get_next_state(move)
+            ended, score = rollout_state.check_endgame()
+        return score
+
+    def backpropagate(self, node, result):
+        curr_node = node
+        while curr_node is not None:
+            curr_node.visits += 1
+            curr_node.wins += result
+            curr_node = curr_node.parent
